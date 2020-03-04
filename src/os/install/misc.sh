38d52bffa86bd30af58f68852aa3cc5c18670f31
Miguel Carvalho <emece.pt@gmail.com>
2020-03-04 14:12:58 +0000

    Fix references to caskroom

--- src/os/install/misc.sh
+++ src/os/install/misc.sh
@@ -41,9 +41,9 @@ brew_install "FFmpeg" "ffmpeg"
 
 print_in_purple "\n   Development Tools\n\n"
 brew_install "Visual Studio Code" "visual-studio-code" "homebrew/cask" "cask"
-brew_install "Dash" "dash" "caskroom/cask" "cask"
-brew_install "iTerm 2" "iterm2" "caskroom/cask" "cask"
-brew_install "Postgres.app" "postgres" "caskroom/cask" "cask"
+brew_install "Dash" "dash" "homebrew/cask" "cask"
+brew_install "iTerm 2" "iterm2" "homebrew/cask" "cask"
+brew_install "Postgres.app" "postgres" "homebrew/cask" "cask"
 brew_install "Yarn" "yarn"
 
 print_in_purple "\n   Browsers\n\n"
