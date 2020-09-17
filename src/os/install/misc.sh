e16cd341cfe4e76191a121ca4c2f71b90ed9f024
Miguel Carvalho <migcarva@users.noreply.github.com>
2020-09-17 18:45:12 +0100

    Add Kap

--- src/os/install/misc.sh
+++ src/os/install/misc.sh
@@ -39,6 +39,7 @@ brew_install "tmux (pasteboard)" "reattach-to-user-namespace"
 print_in_purple "\n   Utils\n\n"
 brew_install "Unarchiver" "the-unarchiver" "homebrew/cask" "cask"
 brew_install "FFmpeg" "ffmpeg"
+brew_install "Kap" "kap"
 
 print_in_purple "\n   Development Tools\n\n"
 brew_install "Visual Studio Code" "visual-studio-code" "homebrew/cask" "cask"
